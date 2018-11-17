03d4341e8e867535710075792ae73f0d4ab51e9d
Max Chechel <hexdigest@gmail.com>
2018-11-18 00:04:15 +0300

    fixed #9

--- templates_tests/interface_with_opentracing.go
+++ templates_tests/interface_with_opentracing.go
@@ -17,30 +17,45 @@ import (
 // TestInterfaceWithTracing implements TestInterface interface instrumented with opentracing spans
 type TestInterfaceWithTracing struct {
 	TestInterface
-	_instance string
+	_instance      string
+	_spanDecorator func(span opentracing.Span, params, results map[string]interface{})
 }
 
 // NewTestInterfaceWithTracing returns TestInterfaceWithTracing
-func NewTestInterfaceWithTracing(base TestInterface, instance string) TestInterfaceWithTracing {
-	return TestInterfaceWithTracing{
+func NewTestInterfaceWithTracing(base TestInterface, instance string, spanDecorator ...func(span opentracing.Span, params, results map[string]interface{})) TestInterfaceWithTracing {
+	d := TestInterfaceWithTracing{
 		TestInterface: base,
 		_instance:     instance,
 	}
+
+	if len(spanDecorator) > 0 && spanDecorator[0] != nil {
+		d._spanDecorator = spanDecorator[0]
+	}
+
+	return d
 }
 
 // F implements TestInterface
 func (_d TestInterfaceWithTracing) F(ctx context.Context, a1 string, a2 ...string) (result1 string, result2 string, err error) {
 	_span, ctx := opentracing.StartSpanFromContext(ctx, _d._instance+".TestInterface.F")
 	defer func() {
-		if err != nil {
+		if _d._spanDecorator != nil {
+			_d._spanDecorator(_span, map[string]interface{}{
+				"ctx": ctx,
+				"a1":  a1,
+				"a2":  a2}, map[string]interface{}{
+				"result1": result1,
+				"result2": result2,
+				"err":     err})
+		} else if err != nil {
 			_ext.Error.Set(_span, true)
 			_span.LogFields(
 				_log.String("event", "error"),
 				_log.String("message", err.Error()),
 			)
 		}
+
 		_span.Finish()
 	}()
-
 	return _d.TestInterface.F(ctx, a1, a2...)
 }
