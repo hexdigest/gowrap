c6122ceca792c03665872a86a39ef60d026e4a58
Max Chechel <hexdigest@gmail.com>
2019-02-06 15:57:02 +0300

    retry template update

--- templates_tests/interface_with_retry.go
+++ templates_tests/interface_with_retry.go
@@ -22,27 +22,26 @@ type TestInterfaceWithRetry struct {
 func NewTestInterfaceWithRetry(base TestInterface, retryCount int, retryInterval time.Duration) TestInterfaceWithRetry {
 	return TestInterfaceWithRetry{
 		TestInterface:  base,
-		_retryCount:    retryCount + 1,
+		_retryCount:    retryCount,
 		_retryInterval: retryInterval,
 	}
 }
 
 // F implements TestInterface
 func (_d TestInterfaceWithRetry) F(ctx context.Context, a1 string, a2 ...string) (result1 string, result2 string, err error) {
+	result1, result2, err = _d.TestInterface.F(ctx, a1, a2...)
+	if err == nil || _d._retryCount < 1 {
+		return
+	}
 	_ticker := time.NewTicker(_d._retryInterval)
 	defer _ticker.Stop()
-	for _i := 0; _i < _d._retryCount; _i++ {
+	for _i := 0; _i < _d._retryCount && err != nil; _i++ {
+		select {
+		case <-ctx.Done():
+			return
+		case <-_ticker.C:
+		}
 		result1, result2, err = _d.TestInterface.F(ctx, a1, a2...)
-		if err == nil {
-			break
-		}
-		if _d._retryCount > 1 {
-			select {
-			case <-ctx.Done():
-				return result1, result2, err
-			case <-_ticker.C:
-			}
-		}
 	}
-	return result1, result2, err
+	return
 }
