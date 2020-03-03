6dcfa0d34561455b87203c7522970d6075b34144
Max Chechel <hexdigest@gmail.com>
2020-03-03 21:49:34 +0300

    Implemented timeout template

---
+++ templates_tests/interface_with_timeout.go
@@ -0,0 +1,40 @@
+package templatestests
+
+// DO NOT EDIT!
+// This code is generated with http://github.com/hexdigest/gowrap tool
+// using ../templates/timeout template
+
+//go:generate gowrap gen -p github.com/hexdigest/gowrap/templates_tests -i TestInterface -t ../templates/timeout -o interface_with_timeout.go
+
+import (
+	"context"
+	"time"
+)
+
+// TestInterfaceWithTimeout implements TestInterface interface instrumented with timeouts
+type TestInterfaceWithTimeout struct {
+	TestInterface
+	config TestInterfaceWithTimeoutConfig
+}
+
+type TestInterfaceWithTimeoutConfig struct {
+	FTimeout time.Duration
+}
+
+// NewTestInterfaceWithTimeout returns TestInterfaceWithTimeout
+func NewTestInterfaceWithTimeout(base TestInterface, config TestInterfaceWithTimeoutConfig) TestInterfaceWithTimeout {
+	return TestInterfaceWithTimeout{
+		TestInterface: base,
+		config:        config,
+	}
+}
+
+// F implements TestInterface
+func (_d TestInterfaceWithTimeout) F(ctx context.Context, a1 string, a2 ...string) (result1 string, result2 string, err error) {
+	var cancelFunc func()
+	if _d.config.FTimeout > 0 {
+		ctx, cancelFunc = context.WithTimeout(ctx, _d.config.FTimeout)
+		defer cancelFunc()
+	}
+	return _d.TestInterface.F(ctx, a1, a2...)
+}
---
+++ templates_tests/interface_with_timeout_test.go
@@ -0,0 +1,50 @@
+package templatestests
+
+import (
+	"context"
+	"testing"
+	"time"
+
+	"github.com/stretchr/testify/assert"
+)
+
+type timeoutsImpl struct {
+	hasTimeout bool
+	t          *testing.T
+}
+
+func (c *timeoutsImpl) F(ctx context.Context, a1 string, a2 ...string) (r1, r2 string, err error) {
+	_, ok := ctx.Deadline()
+	assert.True(c.t, ok == c.hasTimeout)
+
+	return "", "", nil
+}
+
+func (c *timeoutsImpl) NoError(string) string {
+	return ""
+}
+
+func (c *timeoutsImpl) NoParamsOrResults() {
+}
+
+func TestTestInterfaceWithTimeout_F(t *testing.T) {
+	ctx := context.Background()
+
+	t.Run("timeout is not set", func(t *testing.T) {
+		impl := &timeoutsImpl{hasTimeout: false, t: t}
+		wrapped := NewTestInterfaceWithTimeout(impl, TestInterfaceWithTimeoutConfig{})
+
+		_, _, err := wrapped.F(ctx, "")
+		assert.NoError(t, err)
+	})
+
+	t.Run("timeout is set", func(t *testing.T) {
+		impl := &timeoutsImpl{hasTimeout: true, t: t}
+		wrapped := NewTestInterfaceWithTimeout(impl, TestInterfaceWithTimeoutConfig{
+			FTimeout: time.Second,
+		})
+
+		_, _, err := wrapped.F(ctx, "")
+		assert.NoError(t, err)
+	})
+}
