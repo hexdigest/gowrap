3018a0f724fab869923fb4bd33d40c641e06b577
Anton Rutkevich <arutkevich@lyft.com>
2020-08-04 15:34:17 +0300

    Add ability to parametrize prometheus metric name

--- templates_tests/interface.go
+++ templates_tests/interface.go
@@ -9,3 +9,11 @@ type TestInterface interface {
 	NoParamsOrResults()
 	Channels(chA chan bool, chB chan<- bool, chanC <-chan bool)
 }
+
+// AnotherTestInterface is used to test templates where TestInterface was already used
+type AnotherTestInterface interface {
+	F(ctx context.Context, a1 string, a2 ...string) (result1, result2 string, err error)
+	NoError(string) string
+	NoParamsOrResults()
+	Channels(chA chan bool, chB chan<- bool, chanC <-chan bool)
+}
---
+++ templates_tests/interface_with_prometheus_metric_prefix_test.go
@@ -0,0 +1,33 @@
+package templatestests
+
+import (
+	"context"
+	"errors"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestTestInterfaceWithPrometheusMetricPrefix_F(t *testing.T) {
+	t.Run("no error", func(t *testing.T) {
+		impl := &testImpl{r1: "1", r2: "2"}
+
+		wrapped := NewAnotherTestInterfaceWithPrometheus(impl, "test")
+		r1, r2, err := wrapped.F(context.Background(), "a1", "a2")
+
+		assert.NoError(t, err)
+		assert.Equal(t, "1", r1)
+		assert.Equal(t, "2", r2)
+	})
+
+	t.Run("with error", func(t *testing.T) {
+		impl := &testImpl{r1: "1", r2: "2", err: errors.New("unexpected error")}
+
+		wrapped := NewAnotherTestInterfaceWithPrometheus(impl, "test")
+		r1, r2, err := wrapped.F(context.Background(), "a1", "a2")
+
+		assert.Error(t, err)
+		assert.Equal(t, "1", r1)
+		assert.Equal(t, "2", r2)
+	})
+}
