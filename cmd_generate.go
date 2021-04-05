07c47bd816f4080aaf065b6e398a7157524c12c1
Andrey Dovbyshko <dovbysh@gmail.com>
2021-04-05 12:41:14 +0300

    rename snake variable to result

--- cmd_generate.go
+++ cmd_generate.go
@@ -282,9 +282,9 @@ var matchFirstCap = regexp.MustCompile("(.)([A-Z][a-z]+)")
 var matchAllCap = regexp.MustCompile("([a-z0-9])([A-Z])")
 
 func snake(str string) string {
-	snake := matchFirstCap.ReplaceAllString(str, "${1}_${2}")
-	snake = matchAllCap.ReplaceAllString(snake, "${1}_${2}")
-	return strings.ToLower(snake)
+	result := matchFirstCap.ReplaceAllString(str, "${1}_${2}")
+	result = matchAllCap.ReplaceAllString(result, "${1}_${2}")
+	return strings.ToLower(result)
 }
 
 const headerTemplate = `package {{.Package.Name}}
