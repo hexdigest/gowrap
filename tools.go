0a16bbab34a7e0993daa2064c1b54512e6ccbcf9
Max Chechel <hexdigest@gmail.com>
2021-06-21 19:01:54 +0300

    Updated .goreleaser.yml

--- tools.go
+++ tools.go
@@ -5,5 +5,6 @@ package main
 // https://github.com/go-modules-by-example/index/blob/master/010_tools/README.md
 import (
 	_ "github.com/gojuno/minimock/v3/cmd/minimock"
+	_ "github.com/golangci/golangci-lint/cmd/golangci-lint"
 	_ "github.com/goreleaser/goreleaser"
 )
